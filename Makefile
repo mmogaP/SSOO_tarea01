b491159cb755dd87fbec7176550ad6664583ee61
mmogaP <mmoraga.m3@gmail.com>
2020-10-22 23:00:49 -0300

    makefile

---
+++ Makefile
@@ -0,0 +1,8 @@
+CFLAGS = -c
+VPATH = src
+
+main: hora.c
+	gcc -o $@ $?
+
+clean:
+	rm -f main	
--- src/hora.c
+++ src/hora.c
@@ -1,1 +1,11 @@
-//programa en c
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(){
+
+    pid_t pid0;
+
+    printf("Programa hora ejecutandose. PID=%d\n", getppid());
+}
